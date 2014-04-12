3c71c9e1c1023de294792e875cadc2df1619e89c
Sang Mercado <sang.mercado@gmail.com>
2014-04-12 15:30:21 -0500

    Converted mycat to system calls. Still have to change error message to system call

--- src/mycat.c
+++ src/mycat.c
@@ -4,40 +4,31 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-/*int mycat(char *fileName)
-{
-    // Opening a new file stream
-    FILE *file = fopen(fileName, "r");
-    char buffer[MAX_SIZE];
-
-    // Error Checking, perror will display ERRNO as string
-    if (file == NULL) {
-        // Using strerror instead of perror due to formatting reasons
-        printf("mycat: %s: %s\n", fileName, strerror(errno));
-        return -1;
-    }
-    
-    // Might have to refactor this part.
-    while((fgets(buffer, MAX_SIZE, file)) != NULL)
-        printf("%s", buffer);
-    fclose(file);
-    return 0;
-}
-*/
 int mycat(char *fileName)
 {
     int fd = open(fileName, O_RDONLY);
     int bytesRead;
+
+    // MAX_SIZE comes from header file
     char buffer[MAX_SIZE];
+    char errorMsg[MAX_SIZE]; 
 
     // Error Checking, perror will display ERRNO as string
-    if (!fd) {
+    if (fd == -1) {
         // Using strerror instead of perror due to formatting reasons
         printf("mycat: %s: %s\n", fileName, strerror(errno));
         return -1;
     }
 
-    while ((bytesRead = read(fd, buffer, MAX_SIZE)) > 0) {
-        printf("%s", buffer);
-    }
+    while ((bytesRead = read(fd, buffer, MAX_SIZE)) > 0) 
+        write(1, buffer, bytesRead);
+
+    return 0;
+    // There is another case where cat has to take care of two files
+
+}
+
+int main(int argc, char *argv[])
+{
+    mycat(argv[1]);
 }
