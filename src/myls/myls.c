51199625efc2b9217568d81e00d029e597ee1162
sangm <sang.mercado@gmail.com>
2014-04-14 21:01:46 -0500

    Finished prototype of ls. -a option complete

--- src/myls/myls.c
+++ src/myls/myls.c
@@ -6,8 +6,29 @@
 #include <sys/stat.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <dirent.h>
+#include <string.h>
 
-int myls(char *fileName)
+static int SHOW_HIDDEN_FILES = 0;
+static int SHOW_LONG_FORMAT = 0;
+
+int 
+exclude(const struct dirent *a)
+{
+    return (strcmp(a->d_name, ".") && strcmp(a->d_name, "..") 
+        && a->d_name[0] != '.');
+}
+void 
+printDirectory(struct dirent *dirList[], int size)
+{
+    int i;
+    for (i = 0; i < size; ++i)
+        fprintf(stdout, "%s\t",dirList[i]->d_name);
+}
+
+
+int 
+myls(char *fileName)
 {
     struct stat attr;
 
@@ -15,24 +36,50 @@ int myls(char *fileName)
     if (stat(fileName, &attr) < 0)
         return -1;
     if (S_ISDIR(attr.st_mode)) {
-        printf("is directory");
+        struct dirent **dirList;
+        int i, n;
+
+        int (*excludeFunc)(const struct dirent *a);
+        SHOW_HIDDEN_FILES ? (excludeFunc = NULL) : (excludeFunc = exclude);
+        n = scandir(fileName, &dirList, excludeFunc, alphasort);
+
+        if (SHOW_LONG_FORMAT)
+            printf("");
+        else
+            printDirectory(dirList, n);
     }
     else {
-        printf("%s", fileName);
+        printf("%s ", fileName);
     }
 
     return 0;
 }
 
-int main(int argc, char *argv[])
+int 
+main(int argc, char *argv[])
 {
     char *fileName;
-    if (argc == 1) myls(".");
-    else {
-        int i;
-        for (i = 0; i < argc; ++i)
-            myls(argv[i+1]);
+    // This is default...
+//    if (argc == 1) myls(".");
+    int ch;
+    while ((ch = getopt(argc, argv, "al")) != -1) {
+        switch(ch) {
+            case 'a':
+                SHOW_HIDDEN_FILES = 1;
+                break;
+            case 'l':
+                SHOW_LONG_FORMAT = 1;
+                break;
+            default:
+                break;
+                
+        }
     }
+
+    int i;
+    for (i = 0; i < argc; ++i)
+        myls(argv[i+1]);
+
     printf("\n");
     return 0;
 }
